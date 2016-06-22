b341f68837a66d3f3beb9983ff045b6204efd050
Tim Sessions <pdxtim@gmail.com>
2016-06-22 07:56:45 -0700

    Added todo list

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-
+var TodoApp = require('TodoApp');
 
 //Load foundation
 $(document).foundation();
@@ -12,6 +12,6 @@ require('style!css!sass!applicationStyles')
 
 
 ReactDOM.render(
-<p>Boilerplate 3 project</p>,
+<TodoApp/>,
   document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var TodoList = require('TodoList');
+
+var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Clean the yard'
+        }, {
+          id: 3,
+          text: 'Return the modems'
+        }, {
+          id: 4,
+          text: 'Change oil in motorcycle'
+        }
+      ]
+    };
+  },
+  render: function () {
+    var {todos} = this.state;
+    return (
+      <div>
+        <TodoList todos={todos}/>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
