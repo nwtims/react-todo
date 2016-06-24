7959a39058bfac3b8e1a6b530d3c9bc8f6d20317
Tim Sessions <pdxtim@gmail.com>
2016-06-23 18:00:24 -0700

    Added toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,16 +12,20 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Walk the dog'
+          text: 'Walk the dog',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Clean the yard'
+          text: 'Clean the yard',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Return the modems'
+          text: 'Return the modems',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Change oil in motorcycle'
+          text: 'Change oil in motorcycle',
+          completed: true
         }
       ]
     };
@@ -33,10 +37,23 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
+  },
+
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    });
+
+    this.setState({todos: updatedTodos});
   },
 
   handleSearch: function (showCompleted, searchText) {
@@ -52,7 +69,7 @@ var TodoApp = React.createClass({
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     );
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -17,7 +17,26 @@ describe('TodoApp', () => {
 
       todoApp.setState({todos: []});
       todoApp.handleAddTodo(todoText);
-    
+
       expect(todoApp.state.todos[0].text).toBe(todoText);
     });
+
+    it('should toggle completed value when handleToggle is callled', () => {
+      var todoData = {
+        id: 11,
+        text: 'test features',
+        completed: false
+      };
+
+      var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+      todoApp.setState({todos: [todoData]});
+
+      expect(todoApp.state.todos[0].completed).toBe(false);
+      todoApp.handleToggle(11);
+      expect(todoApp.state.todos[0].completed).toBe(true);
+
+    });
+
+
+
   });
